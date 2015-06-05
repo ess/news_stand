9f8bdc07b38cef160388ada9a39c0b3ad6d3e155
Dennis Walters <dwalters@engineyard.com>
2015-06-05 17:29:58 -0400

    Initial commit

---
+++ lib/news_stand.rb
@@ -0,0 +1,5 @@
+require "news_stand/version"
+
+module NewsStand
+  # Your code goes here...
+end
---
+++ spec/news_stand_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe NewsStand do
+  it 'has a version number' do
+    expect(NewsStand::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
